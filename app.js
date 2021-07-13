ca19817cb3210ae601d7e69cd48444afc7c2ccd1
Ahmed <darwish144.ad@gmail.com>
2021-07-13 14:10:01 +0300

    collection creat

--- app.js
+++ app.js
@@ -4,3 +4,23 @@ const Cat = mongoose.model('Cat', { name: String });
 
 const kitty = new Cat({ name: 'Zildjian' });
 kitty.save().then(() => console.log('Hello Darwish'));
+
+// creating new  schema
+
+const fruitSchema = new mongoose.Schema ({
+
+  name: String,
+  rating: Number,
+  review: String
+});
+
+const  Fruit = mongoose.model("Fruit",fruitSchema);
+
+const fruit = new Fruit ({
+
+  name: "Liin balbeemo",
+  rating: 4,
+  review: "Mid aad iyo macaan iyo nafaqo"
+
+});
+fruit.save();
