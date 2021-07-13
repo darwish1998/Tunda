5e228d432b4c7b4b485cbffd413d4e7f0fcc6edd
Ahmed <darwish144.ad@gmail.com>
2021-07-13 21:48:32 +0300

    many insert are  complete

--- app.js
+++ app.js
@@ -1,26 +1,86 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/FruitsDB', {useNewUrlParser: true, useUnifiedTopology: true});
-const Cat = mongoose.model('Cat', { name: String });
+mongoose.connect('mongodb://localhost:27017/FruitsDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-const kitty = new Cat({ name: 'Zildjian' });
+
+
+const Cat = mongoose.model('Cat', {
+  name: String
+});
+
+const kitty = new Cat({
+  name: 'Zildjian'
+});
 kitty.save().then(() => console.log('Hello Darwish'));
 
 // creating new  schema
 
-const fruitSchema = new mongoose.Schema ({
+const fruitSchema = new mongoose.Schema({
 
   name: String,
   rating: Number,
   review: String
 });
 
-const  Fruit = mongoose.model("Fruit",fruitSchema);
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const fruit = new Fruit ({
+const fruit = new Fruit({
 
   name: "Liin balbeemo",
   rating: 4,
   review: "Mid aad iyo macaan iyo nafaqo"
 
 });
-fruit.save();
+
+const avacado = new Fruit({
+
+  name: "avacado",
+  rating: 3,
+  review: "Fatty" v  v 
+
+});
+const banana = new Fruit({
+
+  name: "banana",
+  rating: 10,
+  review: "la dhaafi karo moos"
+
+});
+const tufax = new Fruit({
+
+  name: "tufax",
+  rating: 2,
+  review: "Qaali lkn waa macaan"
+
+});
+
+Fruit.insertMany([avacado, banana, tufax], function(err) {
+  if (err) {
+    console.log(err);
+
+  } else {
+    console.log("Successfully connected to Fruits DataBase");
+  }
+})
+//fruit.save();
+
+
+//challenge
+const PeopleSchema = new mongoose.Schema({
+
+  name: String,
+  age: Number
+
+});
+
+const Person = mongoose.model("Person", PeopleSchema);
+
+const person = new Person({
+
+  name: "Ali",
+  age: 21
+});
+
+person.save();
